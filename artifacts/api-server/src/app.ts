84001d429336d08bfabb9da8b8908f30b8f32895
koricar <augustryan28@gmail.com>
2026-04-18 18:49:43 +0000

    Fix frontend path

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -36,7 +36,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", router);
 
-const frontendPath = path.resolve(__dirname, "../../../korean-car-search/dist/public");
+const frontendPath = path.resolve(__dirname, "../../korean-car-search/dist/public");
 app.use(express.static(frontendPath));
 app.get("*", (_req, res) => {
   res.sendFile(path.join(frontendPath, "index.html"));
