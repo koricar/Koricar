1ec2d4cca4590b8d4515ca0abd4a2dfeb69a778f
koricar <augustryan28@gmail.com>
2026-08-21 00:49:49 +0900

    Update App.tsx

--- artifacts/korean-car-search/src/App.tsx
+++ artifacts/korean-car-search/src/App.tsx
@@ -2,7 +2,6 @@ import { Switch, Route, Router as WouterRouter } from "wouter";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { AlertProvider } from "@/contexts/alert-context";
 
 import Home from "@/pages/home";
 import CarDetails from "@/pages/car-details";
@@ -98,13 +97,13 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
-        <AlertProvider>
+        <>
           <WouterRouter base={import.meta.env.BASE_URL.replace(/\/$/, "")}>
             <Router />
           </WouterRouter>
           <Toaster />
           <WhatsAppButton />
-        </AlertProvider>
+        </>
       </TooltipProvider>
     </QueryClientProvider>
   );
